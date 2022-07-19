c036c8195882194acb30da7c6bed944ae14cd196
ext.roman.mielikhov <ext.roman.mielikhov@kyriba.com>
2022-07-19 11:07:33 +0300

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ToolbarModule } from './shared/components/toolbar/toolbar.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -12,6 +15,8 @@ import { ToolbarModule } from './shared/components/toolbar/toolbar.module';
     ToolbarModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-todo-6964d',
+    appId: '1:246224926761:web:2b2a30e4341a44e60a0438',
+    storageBucket: 'angular-todo-6964d.appspot.com',
+    apiKey: 'AIzaSyDVpU1I1z62RHXfpQwBkJHMfiaeCH9QyzI',
+    authDomain: 'angular-todo-6964d.firebaseapp.com',
+    messagingSenderId: '246224926761',
+  },
   production: true
 };
