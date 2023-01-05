3e4ddb8bb74de4f00bdf34d36d2a6e5c7b2de5db
Roman Mielikhov <roman.mielikhov@iconplc.com>
2023-01-05 10:25:36 +0200

    updated env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   firebase: {
     // databaseURL:
     // 'https://angular-todo-4bce0-default-rtdb.europe-west1.firebasedatabase.app',
-    apiKey: 'AIzaSyDY7pGghHaBQx1dstn6gq0guviSqotv1eY',
-    authDomain: 'angular-todo-6fafd.firebaseapp.com',
-    projectId: 'angular-todo-6fafd',
-    storageBucket: 'angular-todo-6fafd.appspot.com',
-    messagingSenderId: '494601566313',
-    appId: '1:494601566313:web:bf376cb6c8ddd8aaca0af8',
+    apiKey: 'AIzaSyAjd3FFEX3mAmNuLq2Bg5F4y7yOswdCijg',
+    authDomain: 'todo-97f93.firebaseapp.com',
+    projectId: 'todo-97f93',
+    storageBucket: 'todo-97f93.appspot.com',
+    messagingSenderId: '504910311501',
+    appId: '1:504910311501:web:476fa0cf3134bb128fec04',
   },
 
   production: false,
